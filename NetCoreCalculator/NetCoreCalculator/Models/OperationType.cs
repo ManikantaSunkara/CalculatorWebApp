70d8750ecc6d363496a2df4efb2c20d50796043d
ManikantaSunkara <sunkaramanikanta@yahoo.com>
2021-02-19 21:18:53 +0530

    web api code

---
+++ NetCoreCalculator/NetCoreCalculator/Models/OperationType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreCalculator.Models
+{
+    public enum OperationType
+    {
+        Addition,
+        Multiplication,
+        Division,
+        Subtraction,
+        Options
+    }
+}
